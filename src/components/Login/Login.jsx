4e38d1b9ea008828eb77bf532ce779316e0d84a4
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-09-21 16:20:04 +0200

    Add button and handleSubmit to log in

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -12,6 +12,8 @@ import LabelWrapper from "../ui/LabelWrapper/LabelWrapper";
 import StyledInput from "../ui/StyledInput/StyledInput";
 import StyledSpanRegisterLogin from "../ui/StyledSpanRegisterLogin/StyledSpanRegisterLogin";
 import StyledHrefRegisterLogin from "../ui/StyledHrefRegisterLogin/StyledHrefRegisterLogin";
+import Button from "../ui/Button/Button";
+import API_CONFIG from "../../config/api";
 
 const Login = () => {
   const { setAuth } = useAuth();
@@ -31,6 +33,54 @@ const Login = () => {
     setErrMsg("");
   }, [email, pwd]);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `${API_CONFIG.BASE_URL + API_CONFIG.ENDPOINTS.LOGIN}`,
+        JSON.stringify({
+          email: email,
+          password: pwd,
+        }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+          signal: controller.signal,
+        }
+      );
+      //console.log(JSON.stringify(response?.data));
+      //console.log(JSON.stringify(response));
+      const accessToken = response?.data?.accessToken;
+
+      //const roles = response?.data?.roles;
+      //   console.log(roles);
+      setId(JSON.stringify(response?.data.user_id));
+      setAuth({
+        id: JSON.stringify(response?.data.user_id),
+        email,
+        pwd,
+        accessToken,
+      });
+      setEmail("");
+      setPwd("");
+      controller.abort();
+      navigate(from, { replace: true });
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg("No Server Response");
+      } else if (err.response?.status === 400) {
+        setErrMsg("Missing Username or Password");
+      } else if (err.response?.status === 401) {
+        console.log(err);
+
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg("Login Failed");
+      }
+      errRef.current.focus();
+    }
+  };
+
   return (
     <Wrapper>
       <SectionWrapper>
@@ -39,7 +89,7 @@ const Login = () => {
         </ParagraphError>
         <Header>Log in</Header>
         <WrapperSeparator />
-        <FormWrapperRegisterLogin>
+        <FormWrapperRegisterLogin onSubmit={handleSubmit}>
           <LabelWrapper htmlFor="email">Email:</LabelWrapper>
           <StyledInput
             type="email"
@@ -60,6 +110,7 @@ const Login = () => {
             required
             placeholder="put password"
           />
+          <Button>Log in</Button>
         </FormWrapperRegisterLogin>
         <p>
           Need account?
