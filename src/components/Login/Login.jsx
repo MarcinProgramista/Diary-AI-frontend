de7d1777b10d93e7e65732d05080c8098a2243b3
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-09-21 15:23:05 +0200

    Add Wrapper to Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,12 @@
 import { useState, useEffect, useCallback } from "react";
+import Wrapper from "../ui/Wrapper/Wrapper";
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <h1>h</h1>
+    </Wrapper>
+  );
 };
 
 export default Login;
