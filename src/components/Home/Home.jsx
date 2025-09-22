dc5d46dd3872260ad71cd323da1897db274bac52
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-09-22 06:50:49 +0200

    Use function getCategoryFromPath from categoryUtils.js

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,27 +6,17 @@ import Films from "../../assets/video-svgrepo-com.png";
 import Notes from "../../assets/pen-nib-svgrepo-com.png";
 import HomeIcon from "../../assets/home-1-svgrepo-com.png";
 import LogoutIcon from "../../assets/logout-bracket-svgrepo-com.png";
-import {
-  getCategoryColor,
-  getCategoryFromPath,
-} from "../../utils/categoryUtils";
+import { getCategoryFromPath } from "../../utils/categoryUtils";
 import StyledCategory from "../ui/StyledCategory/StyledCategory";
 import ButtonIcon from "../ui/ButtonIcon/ButtonIcon";
 const Home = () => {
-  const positionCategoryAndNameCategory = (path) => {
-    if (path.indexOf("Books") > 0) return [path.indexOf("Books"), "Books"];
-    if (path.indexOf("Films") > 0) return [path.indexOf("Films"), "Films"];
-    if (path.indexOf("Notes") > 0) return [path.indexOf("Notes"), "Notes"];
-    if (path.indexOf("Notes") < 0) return [path.indexOf("Notes"), "Notes"];
-  };
-
-  const categoryName = positionCategoryAndNameCategory(location.pathname)[1];
+  const categoryName = getCategoryFromPath(location.pathname);
   console.log(categoryName);
 
   return (
     <StyledNavbar>
       <StyledParagraph>
-        <StyledLink $category={categoryName} to="/home">
+        <StyledLink to="/home">
           {({ isActive }) => (
             <StyledParagraph>
               <ButtonIcon
