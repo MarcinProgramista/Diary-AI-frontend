84a6ddd90856ab7b351c77d21a50880bb2d0c77a
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-09-23 11:09:45 +0200

    Add useState for notes and buttonShown

--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { getCategoryFromPath } from "../../utils/categoryUtils";
 import { useLocation } from "react-router-dom";
 
 const Notes = () => {
+  const [notes, setNotes] = useState();
+  const [buttonShown, setButtonShown] = useState(false);
   const location = useLocation();
   const categoryName = getCategoryFromPath(location.pathname);
-  console.log(categoryName);
+  //console.log(categoryName);
   return <div></div>;
 };
 
