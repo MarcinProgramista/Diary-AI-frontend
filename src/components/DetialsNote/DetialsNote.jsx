c0d3742d940c1355a39f7646b17169b22bb3615f
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-09-24 14:49:46 +0200

    Import in DetailsNote.jsx useState, useLocation, useAxiosPrivate, useParams

--- src/components/DetialsNote/DetialsNote.jsx
+++ src/components/DetialsNote/DetialsNote.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 
 const DetialsNote = () => {
+  const [note, setNote] = useState();
+  const location = useLocation();
+  const axiosPrivate = useAxiosPrivate();
+  const params = useParams();
+  const note_id = params.id;
+
   return (
     <div>
       <h1>DetialsNote</h1>
