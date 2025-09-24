f73a7006a0a67f833e9d369c8bf2b20e826d85ec
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-09-24 16:05:49 +0200

    Add function CDate  to categoryUtils and use it in DetialsNote.jsx

--- src/components/DetialsNote/DetialsNote.jsx
+++ src/components/DetialsNote/DetialsNote.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
-
+import { CDate, getCategoryFromPath } from "../../utils/categoryUtils";
 const DetialsNote = () => {
   const [note, setNote] = useState();
   const location = useLocation();
--- src/utils/categoryUtils.js
+++ src/utils/categoryUtils.js
@@ -20,3 +20,13 @@ export const getCategoryFromPath = (path) => {
 export const getCategoryColor = (category) => {
   return CATEGORY_COLORS[category] || CATEGORY_COLORS[CATEGORIES.NOTES];
 };
+
+export const CDate = (note) => {
+  if (typeof note?.created === "string") {
+    const date = new Date(note?.created);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return day + "." + month + "." + year;
+  }
+};
